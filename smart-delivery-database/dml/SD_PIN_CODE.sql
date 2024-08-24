36b2c803d66fbf3c3dbfefa71caa8ed4e1e3309d
tanyasharma03 <tasu2612@gmail.com>
2024-08-24 23:03:37 +0530

    Initial DB and FE setup

---
+++ smart-delivery-database/dml/SD_PIN_CODE.sql
@@ -0,0 +1,5 @@
+INSERT INTO SD_PIN_CODE(PIN_CODE,LOCKER_ID,LOCKER_LOCATION) VALUES
+('560034','560034L1','Kendriya Sadan'),
+('560034','560034L2','Koramangala'),
+('560034','560034L3','St. johns medical college'),
+('560034','560034L4','Koramangala I block');
